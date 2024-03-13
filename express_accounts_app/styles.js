7cf631c21d0c1f900154bda3fb9f27570c98e4cc
Corvuscorax888 <137097044+Corvuscorax888@users.noreply.github.com>
2024-03-13 22:41:37 +0000

    colour change

--- express_accounts_app/styles.js
+++ express_accounts_app/styles.js
@@ -8,7 +8,7 @@ export const logoStyles = StyleSheet.create({
       flexDirection: "column",
       alignItems: 'center',
       justifyContent: 'space-around',
-      backgroundColor: "#a60d49"
+      backgroundColor: "##8F093E"
     },
   
     backgroundImage: {
