fbb2359c8f09ae76178b06befb1f37fd0687ba36
Huw Jones <huwjones37@gmail.com>
2024-03-31 14:59:45 +0100

    highlighting of text recognition

--- express_accounts_app/screens/image_picker.js
+++ express_accounts_app/screens/image_picker.js
@@ -1,20 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Image, View, Platform } from 'react-native';
+import { Button, Image, View, Platform, Dimensions, ScrollView } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import MlkitOcr from 'react-native-mlkit-ocr';
 
 
 export default function ImagePicking() {
   const [image, setImage] = useState(null);
+  const [resultFromUri, setResult] = useState(null);
 
   const recognizeTextFromImage = async (result) => {
 
     const resultFromUri = await MlkitOcr.detectFromUri(result.assets[0].uri);
+    setResult(resultFromUri)
 
     {resultFromUri.map((block) => {
       block.lines.map((line) => {
 
-        console.log(line.text)
+        // console.log(line.text)
  
       });
     })}
@@ -23,6 +25,18 @@ export default function ImagePicking() {
 
 
   };
+
+  function fitWidth(value, imageWidth) {
+    const fullWidth = Dimensions.get('window').width;
+    console.log(image)
+    if (image) {return (value / imageWidth) * fullWidth;}
+    // 
+  }
+  
+  function fitHeight(value, imageHeight) {
+    const fullHeight = Dimensions.get('window').height;
+    if (image) {return (value / imageHeight) * fullHeight};
+  }
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
@@ -40,14 +54,44 @@ export default function ImagePicking() {
 
     if (!result.canceled) {
       await recognizeTextFromImage(result)
-      setImage(result.assets[0].uri);
+      setImage(result.assets[0]);
     }
   };
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Button title="Pick an image fm camera roll" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} style={{ width: 350, height: 600 }} />}
+
+      <ScrollView>
+        <View>        
+          {image && <Image source={{ uri: image.uri }} style={{ width: Dimensions.get('window').width, height: Dimensions.get('window').height }} />}
+          {resultFromUri?.map((block) => {
+                return block.lines.map((line) => {
+                  return (
+                    <View
+                      key={line.text}
+                      style={{
+                        backgroundColor: '#706464af',
+                        position: 'absolute',
+                        // top: line.bounding.top,
+                        // height: line.bounding.height,
+                        // left: line.bounding.left, 
+                        // width: line.bounding.width,
+                        
+                        top: fitHeight(line.bounding.top, image?.height ?? 0),
+                        height: fitHeight(line.bounding.height, image?.height ?? 0),
+                        left: fitWidth(line.bounding.left, image?.width ?? 0),
+                        width: fitWidth(line.bounding.width, image?.width ?? 0),
+                      }}
+                    >
+                      {/* <Text style={{ fontSize: 10 }}>{line.text}</Text> */}
+                    </View>
+                  );
+                });
+              })}
+
+        </View>
+       </ScrollView>  
     </View>
   );
 }
