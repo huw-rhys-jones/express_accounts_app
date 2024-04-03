565772820351965368007fd2d037c913263e225e
Huw Jones <huwjones37@gmail.com>
2024-04-04 00:16:33 +0100

    restarted camera feature

--- express_accounts_app/App.js
+++ express_accounts_app/App.js
@@ -7,18 +7,20 @@ import Login from './screens/login'
 import NewUser from './screens/create_new_account';
 import ReceiptData from './screens/receipt_data'
 import ImagePicking from './screens/image_picker'
+import Cam from './screens/camera';
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="ReceiptData">
+      <Stack.Navigator initialRouteName="Camera">
         <Stack.Screen options={{ headerShown: false }} name="Front" component={Front} />
         <Stack.Screen options={{ headerShown: false }} name="Login" component={Login} />
         <Stack.Screen options={{ headerShown: false }} name="NewUser" component={NewUser} />
         <Stack.Screen options={{ headerShown: false }} name="ReceiptData" component={ReceiptData} />
-        <Stack.Screen options={{ headerShown: false }} name="picking" component={ImagePicking} />
+        <Stack.Screen options={{ headerShown: false }} name="Picking" component={ImagePicking} />
+        <Stack.Screen options={{ headerShown: false }} name="Camera" component={Cam} />
       </Stack.Navigator>
     </NavigationContainer>
   );
